407c8936bf001762efd766ccc9507b1b6b65ec68
Jimisi Rajasombat <70145646+jrajasombat@users.noreply.github.com>
2022-07-15 22:55:31 -0700

    Add files via upload

--- baseball_predictor_st.py
+++ baseball_predictor_st.py
@@ -32,13 +32,40 @@ st.title('Predicting MLB winning probabilities')
 st.markdown('## ')
 
 # Initiate sidebar
-st.sidebar.markdown('## Select Parameters')
+st.sidebar.markdown('## ENTER PARAMETERS')
+
+# Logo in the sidebar
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+@st.cache(allow_output_mutation=True)
+def get_img_with_href(local_img_path, target_url):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}" target="_blank">
+            <img src="data:image/{img_format};base64,{bin_str}" width = "75" />
+        </a>'''
+    return html_code
+
+png_html = get_img_with_href('n.png', 'https://www.jimisinith.com')
+
+col1, col2, col3 = st.sidebar.beta_columns([3,7,1])
+with col1:
+    st.write("")
+with col2:
+    st.markdown(png_html, unsafe_allow_html=True)
+with col3:
+    st.write("")
+
 
 # Enter inputs
-
 count = 0
 
-your_team = st.sidebar.selectbox('Select your team', (
+your_team = st.sidebar.selectbox('## Select your team', (
 'ARI',
 'ATL',
 'BOS',
@@ -80,7 +107,13 @@ your_pitcher = st.sidebar.text_input('Who is their starting pitcher?', value = '
 
 count += 1
 
-opp_team = st.sidebar.selectbox('Select the opposing team', (
+your_odds = st.sidebar.number_input('Enter decimal odds for your team', value = 1.69, key = count)
+
+count += 1
+
+st.sidebar.markdown('## ')
+
+opp_team = st.sidebar.selectbox('## Select the opposing team', (
 'ARI',
 'ATL',
 'BOS',
@@ -118,11 +151,22 @@ opp_pitcher = st.sidebar.text_input('Who is their starting pitcher?', value = 'G
 
 count += 1
 
-your_odds = st.sidebar.number_input('Enter decimal odds for your team', value = 1.69, key = count)
-
-count += 1
-
 opp_odds = st.sidebar.number_input('Enter decimal odds for the opposing team', value = 2.25, key = count)
+
+# Configure sidebar
+st.sidebar.markdown('## ')
+st.sidebar.markdown('## ')
+st.sidebar.markdown('## Discussion')
+st.sidebar.markdown('* One approach to reduce risk when constructing an investment portfolio \
+is __maximum diversification portfolio optimization__. In particular, this strategy maximizes \
+a diversification ratio and does not take into account expected returns.')
+st.sidebar.markdown('* The __diversification ratio__ is defined as the ratio of the weighted average \
+of all the volatilites in the portfolio divided by the total portfolio volatility.')
+st.sidebar.markdown('* This web app optimizes a portfolio of stocks, bonds, and gold at regular \
+intervals.')
+st.sidebar.markdown('## ')
+st.sidebar.markdown('## ')
+
 
 if your_home == 'Yes':
     home_team = your_team
@@ -246,7 +290,7 @@ print(str(datetime.now().ctime()))
 
 summary = results_df[['Classifier','Train Accuracy','Test Accuracy']].groupby(['Classifier']).mean()
 summary['Composite Score'] = summary['Train Accuracy'] * 0.25 + summary['Test Accuracy']
-display(summary)
+
 for i in range(len(summary)):
     if summary.iloc[i]['Composite Score'] == summary['Composite Score'].max():
         best_model_home = summary.iloc[i].name
@@ -433,7 +477,7 @@ print(str(datetime.now().ctime()))
 
 summary = results_df[['Classifier','Train Accuracy','Test Accuracy']].groupby(['Classifier']).mean()
 summary['Composite Score'] = summary['Train Accuracy'] * 0.25 + summary['Test Accuracy']
-display(summary)
+
 for i in range(len(summary)):
     if summary.iloc[i]['Composite Score'] == summary['Composite Score'].max():
         best_model_away = summary.iloc[i].name
@@ -557,47 +601,3 @@ if your_team == away_team:
 
 print('')
 print('')
-
-#reb.rebalance(coi)
-
-# Configure sidebar
-st.sidebar.markdown('## ')
-st.sidebar.markdown('## ')
-st.sidebar.markdown('## Discussion')
-st.sidebar.markdown('* One approach to reduce risk when constructing an investment portfolio \
-is __maximum diversification portfolio optimization__. In particular, this strategy maximizes \
-a diversification ratio and does not take into account expected returns.')
-st.sidebar.markdown('* The __diversification ratio__ is defined as the ratio of the weighted average \
-of all the volatilites in the portfolio divided by the total portfolio volatility.')
-st.sidebar.markdown('* This web app optimizes a portfolio of stocks, bonds, and gold at regular \
-intervals.')
-st.sidebar.markdown('## ')
-st.sidebar.markdown('## ')
-
-
-# Logo in the sidebar
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-@st.cache(allow_output_mutation=True)
-def get_img_with_href(local_img_path, target_url):
-    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
-    bin_str = get_base64_of_bin_file(local_img_path)
-    html_code = f'''
-        <a href="{target_url}" target="_blank">
-            <img src="data:image/{img_format};base64,{bin_str}" width = "75" />
-        </a>'''
-    return html_code
-
-png_html = get_img_with_href('n.png', 'https://www.jimisinith.com')
-
-col1, col2, col3 = st.sidebar.beta_columns([3,7,1])
-with col1:
-    st.write("")
-with col2:
-    st.markdown(png_html, unsafe_allow_html=True)
-with col3:
-    st.write("")
